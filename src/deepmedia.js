c5e30ee834ae29e15a8c1923b2226b0c9f620256
Liby99 <liby99@icloud.com>
2016-12-06 19:40:18 -0800

    Ready to deploy

---
+++ src/deepmedia.js
@@ -0,0 +1,67 @@
+var express = require("express");
+var config = require("./server/data/config.json");
+
+function setDateTime() {
+    require("./server/module/datetime.js")();
+}
+
+function setCookie(server) {
+    server.use(require("cookie-parser")());
+    server.use(require("./server/api/cookie.js"));
+}
+
+function setRenderEngine(server) {
+    server.set("views", __dirname + "/public");
+    server.engine(".html", require("ejs").__express);
+    server.set('view engine', "html");
+}
+
+function setRoute(server) {
+    setDefaultPage(server);
+    setStaticField(server);
+    setPageHandler(server);
+}
+
+function setDefaultPage(server) {
+    server.get("/", function (req, res) {
+        res.redirect("/" + config["default_page"]);
+    });
+}
+
+function setStaticField(server) {
+    config["static_field"].forEach(function (obj, number) {
+        server.use(obj, express.static(__dirname + "/public/" + obj));
+    });
+}
+
+function setPageHandler(server) {
+    require("./server/module/route.js").set(server);
+}
+
+function setAjax(server) {
+    require("./server/module/ajax.js").set(server);
+}
+
+(function () {
+    var server = express();
+    
+    setDateTime();
+    setCookie(server);
+    setRenderEngine(server);
+    setRoute(server);
+    setAjax(server);
+    
+    server.use(function (err, req, res, next) {
+        console.log("Server Error: " + err);
+        next(err);
+    });
+    
+    server.listen(config['port'], function (err) {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            console.log(config['name'] + ' Server Now Listening to Port ' + config['port']);
+        }
+    });
+})();
