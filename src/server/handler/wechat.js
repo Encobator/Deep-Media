d257315b8533c1a9b9e75b293ba3e4f733620c13
Liby99 <liby99@icloud.com>
2016-12-21 01:16:43 +0800

    Done wechat subscription

--- src/server/handler/wechat.js
+++ src/server/handler/wechat.js
@@ -1,24 +1,50 @@
-var wechat = require("../api/wechat.js");
+var Wechat = require("../api/wechat.js");
+var User = require("../api/user.js");
 var xml2js = require("xml2js");
+var parseXml = require('xml2js').parseString;
 
 module.exports = {
     process: function (req, res) {
         if (req.method == "GET") {
-            if (wechat.verify(req.query["signature"], req.query["timestamp"], req.query["nonce"])) {
+            if (Wechat.verify(req.query["signature"], req.query["timestamp"], req.query["nonce"])) {
                 res.write(req.query["echostr"]);
             }
             res.end();
         }
         else if (req.method == "POST") {
-            var data = xml2js.parse(req.body);
-            switch (data.event) {
-                case "text":
-                case "subscribe": break;
-                case "button":
-                    switch (data.key) {
-                        case
+            parseXml(req.body, function (err, result) {
+                if (err) {
+                    res.end();
+                }
+                else {
+                    switch (result.MsgType) {
+                    case "text":
+                        var content = result.Content;
+                        break;
+                    case "event":
+                        switch (result.Event) {
+                        case "subscribe":
+                            var openId = result.FromUserName;
+                            User.newUser(openId, function (info) {
+                                if (info) {
+                                    res.write(info["nickname"] + "，感谢您的关注～您可以点击下方的菜单查看我们的原创作品");
+                                }
+                                res.end();
+                            });
+                            break;
+                        case "unsubscribe":
+                            res.end();
+                            break;
+                        case "CLICK":
+                            switch (result.EventKey) {
+                                case "": break;
+                            }
+                            break;
+                        }
+                        break;
                     }
-            }
+                }
+            });
         }
         else {
             res.end();
