0adadaa9bb182e1b398bf7b5df3cc44cc9950368
Yuhao He <yuhaohe@usc.edu>
2016-12-20 23:18:00 +0800

    working on review

--- src/server/route/admin/review.js
+++ src/server/route/admin/review.js
@@ -1,12 +1,20 @@
+var mysql = require("../../module/mysql.js");
+var User = require("../../api/user.js");
 
 module.exports = function (req, res, callback) {
-  var User = require("../../api/user.js");
-  User.verify(req, res, function () {
-      Review.getAllReview(function (reviews) {
-          callback({
-              "reviews": reviews
-          });
-      })
-
+  User.verify(req, res, function(){
+      mysql.query("SELECT * FROM comment", function(err, result){
+          if (err){
+              console.log("Error selecting: " + err);
+          } else {
+            if (result.length <= 0){
+              callback(null);
+            }
+            else {
+              callback({"reviews": result});
+            }
+            //res.render('home', {page_title:'Barry\'s personal anydo', data: rows});
+          }
+      });
   });
 }
