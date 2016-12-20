278d5c6eb5ed7e1b0924856d3ced4f957a4f6284
Liby99 <liby99@icloud.com>
2016-12-20 19:14:37 +0800

    Add admin panels

--- src/server/handler/wechat.js
+++ src/server/handler/wechat.js
@@ -1,10 +1,11 @@
 var wechat = require("../api/wechat.js");
+var xml2js = require("xml2js");
 
 module.exports = {
     process: function (req, res) {
         if (req.method == "GET") {
             
-            console.log(req.query);
+            // console.log(req.query);
             
             if (wechat.verify(req.query["signature"], req.query["timestamp"], req.query["nonce"])) {
                 res.write(req.query["echostr"]);
@@ -12,7 +13,15 @@ module.exports = {
             res.end();
         }
         else if (req.method == "POST") {
-            res.end();
+            var data = xml2js.parse(req.body);
+            switch (data.event) {
+                case "text":
+                case "subscribe": break;
+                case "button":
+                    switch (data.key) {
+                        case 
+                    }
+            }
         }
         else {
             res.end();
