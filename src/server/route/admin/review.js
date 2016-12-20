01b6ff6d01e9ee6a7e36afd6cb76f988680d66a9
Yuhao He <yuhaohe@usc.edu>
2016-12-20 23:48:24 +0800

    edited the titles

--- src/server/route/admin/review.js
+++ src/server/route/admin/review.js
@@ -3,17 +3,12 @@ var User = require("../../api/user.js");
 
 module.exports = function (req, res, callback) {
   User.verify(req, res, function(){
-      mysql.query("SELECT * FROM comment", function(err, result){
-          if (err){
-              console.log("Error selecting: " + err);
-          } else {
-            if (result.length <= 0){
+      Review.getAllReviews(function(result){
+          if (result.length <= 0){
               callback(null);
-            }
-            else {
+          }
+          else {
               callback({"reviews": result});
-            }
-            //res.render('home', {page_title:'Barry\'s personal anydo', data: rows});
           }
       });
   });
