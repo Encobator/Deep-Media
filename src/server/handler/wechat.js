30e9c1c8f1b6f4896d344b142528f4f64fa26cab
Liby99 <liby99@icloud.com>
2016-12-21 10:07:20 +0800

    One more try

--- src/server/handler/wechat.js
+++ src/server/handler/wechat.js
@@ -1,9 +1,124 @@
+var ejs = require("ejs");
+var xml2js = require("xml2js");
 var Wechat = require("../api/wechat.js");
 var User = require("../api/user.js");
 var config = require("../data/config.json");
-var xml2js = require("xml2js");
 var parseXml = xml2js.parseString;
 var toXml = new xml2js.Builder().buildObject;
+
+var tpl = ['<xml>',
+    '<ToUserName><![CDATA[<%-toUsername%>]]></ToUserName>',
+    '<FromUserName><![CDATA[<%-fromUsername%>]]></FromUserName>',
+    '<CreateTime><%=createTime%></CreateTime>',
+    '<% if (msgType === "device_event" && (Event === "subscribe_status" || Event === "unsubscribe_status")) { %>',
+      '<% if (Event === "subscribe_status" || Event === "unsubscribe_status") { %>',
+        '<MsgType><![CDATA[device_status]]></MsgType>',
+        '<DeviceStatus><%=DeviceStatus%></DeviceStatus>',
+      '<% } else { %>',
+        '<MsgType><![CDATA[<%=msgType%>]]></MsgType>',
+        '<Event><![CDATA[<%-Event%>]]></Event>',
+      '<% } %>',
+    '<% } else { %>',
+      '<MsgType><![CDATA[<%=msgType%>]]></MsgType>',
+    '<% } %>',
+  '<% if (msgType === "news") { %>',
+    '<ArticleCount><%=content.length%></ArticleCount>',
+    '<Articles>',
+    '<% content.forEach(function(item){ %>',
+      '<item>',
+        '<Title><![CDATA[<%-item.title%>]]></Title>',
+        '<Description><![CDATA[<%-item.description%>]]></Description>',
+        '<PicUrl><![CDATA[<%-item.picUrl || item.picurl || item.pic %>]]></PicUrl>',
+        '<Url><![CDATA[<%-item.url%>]]></Url>',
+      '</item>',
+    '<% }); %>',
+    '</Articles>',
+  '<% } else if (msgType === "music") { %>',
+    '<Music>',
+      '<Title><![CDATA[<%-content.title%>]]></Title>',
+      '<Description><![CDATA[<%-content.description%>]]></Description>',
+      '<MusicUrl><![CDATA[<%-content.musicUrl || content.url %>]]></MusicUrl>',
+      '<HQMusicUrl><![CDATA[<%-content.hqMusicUrl || content.hqUrl %>]]></HQMusicUrl>',
+      '<% if (content.thumbMediaId) { %> ',
+      '<ThumbMediaId><![CDATA[<%-content.thumbMediaId || content.mediaId %>]]></ThumbMediaId>',
+      '<% } %>',
+    '</Music>',
+  '<% } else if (msgType === "voice") { %>',
+    '<Voice>',
+      '<MediaId><![CDATA[<%-content.mediaId%>]]></MediaId>',
+    '</Voice>',
+  '<% } else if (msgType === "image") { %>',
+    '<Image>',
+      '<MediaId><![CDATA[<%-content.mediaId%>]]></MediaId>',
+    '</Image>',
+  '<% } else if (msgType === "video") { %>',
+    '<Video>',
+      '<MediaId><![CDATA[<%-content.mediaId%>]]></MediaId>',
+      '<Title><![CDATA[<%-content.title%>]]></Title>',
+      '<Description><![CDATA[<%-content.description%>]]></Description>',
+    '</Video>',
+  '<% } else if (msgType === "hardware") { %>',
+    '<HardWare>',
+      '<MessageView><![CDATA[<%-HardWare.MessageView%>]]></MessageView>',
+      '<MessageAction><![CDATA[<%-HardWare.MessageAction%>]]></MessageAction>',
+    '</HardWare>',
+    '<FuncFlag>0</FuncFlag>',
+  '<% } else if (msgType === "device_text" || msgType === "device_event") { %>',
+    '<DeviceType><![CDATA[<%-DeviceType%>]]></DeviceType>',
+    '<DeviceID><![CDATA[<%-DeviceID%>]]></DeviceID>',
+    '<% if (msgType === "device_text") { %>',
+      '<Content><![CDATA[<%-content%>]]></Content>',
+    '<% } else if ((msgType === "device_event" && Event != "subscribe_status" && Event != "unsubscribe_status")) { %>',
+      '<Content><![CDATA[<%-content%>]]></Content>',
+      '<Event><![CDATA[<%-Event%>]]></Event>',
+    '<% } %>',
+      '<SessionID><%=SessionID%></SessionID>',
+  '<% } else if (msgType === "transfer_customer_service") { %>',
+    '<% if (content && content.kfAccount) { %>',
+      '<TransInfo>',
+        '<KfAccount><![CDATA[<%-content.kfAccount%>]]></KfAccount>',
+      '</TransInfo>',
+    '<% } %>',
+  '<% } else { %>',
+    '<Content><![CDATA[<%-content%>]]></Content>',
+  '<% } %>',
+  '</xml>'].join('');
+
+/*!
+ * 编译过后的模版
+ */
+var compiled = ejs.compile(tpl);
+
+function formatMessage(result) {
+    result = result.xml;
+    var message = {};
+    if (typeof result === 'object') {
+        for (var key in result) {
+            if (!(result[key] instanceof Array) || result[key].length === 0) {
+                continue;
+            }
+            if (result[key].length === 1) {
+                var val = result[key][0];
+                if (typeof val === 'object') {
+                    message[key] = formatMessage(val);
+                }
+                else {
+                    message[key] = (val || '').trim();
+                }
+            }
+            else {
+                message[key] = [];
+                result[key].forEach(function (item) {
+                    message[key].push(formatMessage(item));
+                });
+            }
+        }
+        return message;
+    }
+    else {
+        return result;
+    }
+}
 
 module.exports = {
     process: function (req, res) {
@@ -22,7 +137,7 @@ module.exports = {
             console.log("RECEIVED MESSAGE!!!");
             console.log(req.rawbody);
             
-            parseXml(req.rawbody, function (err, result) {
+            parseXml(req.rawbody, {trim: true}, function (err, result) {
                 if (err) {
                     
                     console.log(err);
@@ -33,10 +148,7 @@ module.exports = {
                     
                     console.log(result);
                     
-                    result = result.xml;
-                    for (var i in result) {
-                        result[i] = result[i][0];
-                    }
+                    result = formatMessage(result);
                     
                     console.log(result);
                     
@@ -45,13 +157,13 @@ module.exports = {
                         var content = result.Content;
                         var openId = result.FromUserName;
                         var reply = {
-                            "ToUserName": openId,
-                            "FromUserName": config["wechat_id"],
-                            "CreateTime": (new Date()).getTime(),
-                            "MsgType": "text",
-                            "Content": "你个傻逼"
+                            "toUserName": openId,
+                            "fromUserName": config["wechat_id"],
+                            "createTime": (new Date()).getTime(),
+                            "msgType": "text",
+                            "content": "你个傻逼"
                         }
-                        var xml = toXml(reply);
+                        var xml = compiled(reply);
                         
                         console.log("reply: " + xml);
                         
