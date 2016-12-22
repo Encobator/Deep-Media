d563f22d40d3f1f01ce1340de68c825f602acd51
Liby99 <liby99@icloud.com>
2016-12-22 10:18:58 +0800

    Add error log

--- src/server/api/user.js
+++ src/server/api/user.js
@@ -310,6 +310,7 @@ module.exports = {
             username
         ], function (err, result) {
             if (err) {
+                console.log(err);
                 callback(3);
             }
             else {
