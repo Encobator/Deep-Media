7955b81f44e7aac450f5b5f27f4f7b6b4bcdf5c7
Yuhao He <yuhaohe@usc.edu>
2016-12-28 17:13:16 +0800

    edited news.js

--- src/server/api/news.js
+++ src/server/api/news.js
@@ -2,7 +2,7 @@ var mysql = require("../module/mysql.js");
 
 module.exports = {
     getAllNews: function (callback) {
-        mysql.query("SELECT * FROM `v9_news`", {}, function (err, result) {
+        mysql.query("SELECT * FROM `v9_news` ORDER BY `updatetime` DESC", {}, function (err, result) {
             if (err) {
                 console.log(err);
                 callback(null);
