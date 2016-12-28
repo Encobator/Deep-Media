278dcc2ee3ad355306b9b63c5570350be532fe35
Liby99 <liby99@icloud.com>
2016-12-28 23:21:41 +0800

    Done actor image upload

--- src/server/api/actor.js
+++ src/server/api/actor.js
@@ -73,7 +73,7 @@ module.exports = {
                             phone,
                             role,
                             intro,
-                            image,
+                            "img/user/" + UUID + ".jpg",
                             UUID
                         ], function (err, result) {
                             if (err) {
