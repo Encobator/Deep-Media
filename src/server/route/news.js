8c1372a1fa095891dd4a54f59fab550bf734f15f
Yuhao He <yuhaohe@usc.edu>
2016-12-28 18:17:41 +0800

    fixed 404 bug

--- src/server/route/news.js
+++ src/server/route/news.js
@@ -3,11 +3,9 @@ var User = require("../api/user.js");
 var News = require("../api/news.js");
 
 module.exports = function (req, res, callback) {
-    User.verify(req, res, function () {
-        News.getAllNews(function (result) {
-            callback({
-                "events": result
-            });
+    News.getAllNews(function (result) {
+        callback({
+            "events": result
         });
     });
 }
