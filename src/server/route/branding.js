8c29d144e0f9cec2223814fdbceba9c8e263dcce
Yuhao He <yuhaohe@usc.edu>
2016-12-28 22:00:20 +0800

    y

--- src/server/route/branding.js
+++ src/server/route/branding.js
@@ -3,7 +3,6 @@ var User = require("../api/user.js");
 
 module.exports = function (req, res, callback) {
   User.verify(req, res, function () {
-    console.log("shit");
     callback({});
   });
     /*Branding.getAllCases(function (result) {
