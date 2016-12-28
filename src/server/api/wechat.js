811f90985b58e4145a70ee9f68c8e86ff12837b0
Liby99 <liby99@icloud.com>
2016-12-28 15:02:13 +0800

    Add log

--- src/server/api/wechat.js
+++ src/server/api/wechat.js
@@ -9,6 +9,7 @@ function generateUri(page) {
 
 function generateWechatRedirectURI(uri) {
     var url = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=" + config["wechat_appid"] + "&redirect_uri=" + encodeURIComponent(uri) + "&response_type=code&scope=snsapi_base#wechat_redirect";
+    console.log(url);
     return url;
 }
 
